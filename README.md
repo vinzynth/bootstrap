7797365879b1826f0096dcc0f0d5e4ade48d50cc
root <root@localhost.localdomain>
2015-09-27 18:12:01 +0000

    Added Readme Added init script for worker

---
+++ README.md
@@ -0,0 +1,3 @@
+# ChRL - Bootstrap
+
+Bootstrap Repository
---
+++ apt-get/worker.sh
@@ -0,0 +1,6 @@
+apt-get update
+apt-get upgrade
+
+apt-get install git -y
+apt-get install subversion -y
+apt-get install oracle-java8-installer -y
