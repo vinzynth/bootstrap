5e0243167af5f62240a038e0598713b881078597
root <root@localhost.localdomain>
2015-09-27 18:13:54 +0000

    added force yes parameter
    added docker
    added gradle

--- apt-get/worker.sh
+++ apt-get/worker.sh
@@ -1,6 +1,9 @@
 apt-get update
 apt-get upgrade
 
-apt-get install git -y
-apt-get install subversion -y
-apt-get install oracle-java8-installer -y
+apt-get install docker -y --force-yes
+apt-get install git -y --force-yes
+apt-get install subversion -y --force-yes
+apt-get install oracle-java8-installer -y --force-yes
+apt-get install maven -y --force-yes
+apt-get install gradle -y --force-yes
